896be248155f4dc7b22c10ec458c96520e1a1f4b
jiu685 <1580841365@qq.com>
2026-04-06 18:03:11 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.No)
+    basic.pause(200)
+    basic.showIcon(IconNames.Confused)
+    basic.pause(200)
+    basic.showIcon(IconNames.TShirt)
+})
